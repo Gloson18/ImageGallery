ad764035d2668bf33d0ec801cd8ecaf499649b41
Gloson <jiuzhangcoding@gmail.com>
2018-10-08 21:04:39 -0700

    Test Version

---
+++ Color Game/Color Game.js
@@ -0,0 +1,115 @@
+// The space between the next number and the last comma is really important, missing will lead bug
+var numberOfSquares = 6;
+var colors = generateRandomColors(numberOfSquares);
+var squares = document.querySelectorAll(".square");
+var pickedColor = pickColor();
+var colorDisplay = document.getElementById("colorDisplay");
+var messageDisplay = document.querySelector("#message");
+var h1 = document.querySelector("h1");
+var resetButton = document.querySelector("#reset");
+var easyBtn = document.querySelector("#easyBtn");
+var hardBtn = document.querySelector("#hardBtn");
+
+colorDisplay.textContent = pickedColor;
+
+for (var i = 0; i < squares.length; i++) {
+	//add initial colors to squares
+	squares[i].style.backgroundColor = colors[i];
+	//add click listeners to squares
+	squares[i].addEventListener("click", function() {
+		//grab color of clicked square
+		var clickedColor = this.style.backgroundColor;
+		//compare color to pickedcolor
+		if (clickedColor === pickedColor) {
+			messageDisplay.textContent = "Correct!";
+			resetButton.textContent = "Play Again?"
+			changeColors(clickedColor);
+			h1.style.backgroundColor = clickedColor;
+		}
+		else {
+			this.style.backgroundColor = "#232323";
+			messageDisplay.textContent = "Try Again!";
+		}
+	});
+}
+
+resetButton.addEventListener("click", function() {
+	//generate all new colors
+	colors = generateRandomColors(numberOfSquares);
+	//pick a new random color from array
+	pickedColor = pickColor();
+	//change colorDisplay to match the new picked color
+	colorDisplay.textContent = pickedColor;
+	messageDisplay.textContent = "";
+	this.textContent = "new colors";
+	//change colors of squares
+	for (var i = 0; i < squares.length; i++) {
+		squares[i].style.backgroundColor = colors[i];
+	}
+	h1.style.backgroundColor = "steelblue";
+});
+
+easyBtn.addEventListener("click", function() {
+	hardBtn.classList.remove("selected");
+	easyBtn.classList.add("selected");
+	numberOfSquares = 3;
+	colors = generateRandomColors(numberOfSquares);
+	pickedColor = pickColor();
+	colorDisplay.textContent = pickedColor;
+	for (var i = 0; i < squares.length; i++) {
+		if (colors[i]) {
+			squares[i].style.backgroundColor = colors[i];
+		}
+		else {
+			squares[i].style.display = "none";
+		}
+	}
+});
+
+hardBtn.addEventListener("click", function() {
+	easyBtn.classList.remove("selected");
+	hardBtn.classList.add("selected");
+	numberOfSquares = 6;
+	colors = generateRandomColors(numberOfSquares);
+	pickedColor = pickColor();
+	colorDisplay.textContent = pickedColor;
+	for (var i = 0; i < squares.length; i++) {
+		squares[i].style.backgroundColor = colors[i];
+		squares[i].style.display = "block";
+	}
+});
+
+function changeColors(color) {
+	//loop through all squares
+	for (var i = 0; i < squares.length; i++) {
+		//change each color to match given color
+		squares[i].style.backgroundColor = color;
+	}
+}
+
+function pickColor() {
+	//Math.floor -> only keep the integer part
+	//Math.random -> generate a number not smaller than 0 but smaller than 1
+	var random = Math.floor(Math.random() * colors.length);
+	return colors[random];
+}
+
+function generateRandomColors(num) {
+	//maka an array
+	var arr = [];
+	//add random colors to array
+	for (var i = 0; i < num; i++) {
+		//get random color and then push it into arr
+		arr.push(randomColor());
+	}
+
+	//return this array	
+	return arr;
+}
+
+function randomColor() {
+	var r = Math.floor(Math.random() * 256);
+	var g = Math.floor(Math.random() * 256);
+	var b = Math.floor(Math.random() * 256);
+	return "rgb(" + r + ", " + g + ", " + b + ")";
+}
